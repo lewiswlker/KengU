efde995e7d011710aff69c7efce43b8c36248a3e
melldy <100991941+melldy@users.noreply.github.com>
2025-11-06 22:46:04 +0800

    add UserDAO

--- dao/__init__.py
+++ dao/__init__.py
@@ -6,10 +6,12 @@ from .config import DatabaseConfig
 from .connector import DBConnector
 from .course import CourseDAO
 from .usercourse import UserCourseDAO
+from .user import UserDAO
 
 __all__ = [
     "DatabaseConfig",
     "DBConnector",
     "CourseDAO",
     "UserCourseDAO",
+    "UserDAO",
 ]
